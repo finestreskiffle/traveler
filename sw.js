c04cff78e7862c47227c50a317fda0bc4af24359
finestreskiffle <finestre_03skiffle@icloud.com>
2025-11-26 16:36:42 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Travel Manager Service Worker v3.0.5
-const CACHE_VERSION = 'v3.0.5';
+// Travel Manager Service Worker v3.1.0
+const CACHE_VERSION = 'v3.1.0';
 const STATIC_CACHE = `travel-manager-static-${CACHE_VERSION}`;
 const DYNAMIC_CACHE = `travel-manager-dynamic-${CACHE_VERSION}`;
 
@@ -27,7 +27,7 @@ const API_DOMAINS = [
 
 // Install: Cache static assets
 self.addEventListener('install', (event) => {
-    console.log('[SW] Installing Service Worker v3.0.5');
+    console.log('[SW] Installing Service Worker v3.1.0');
     event.waitUntil(
         caches.open(STATIC_CACHE)
             .then((cache) => {
@@ -40,7 +40,7 @@ self.addEventListener('install', (event) => {
 
 // Activate: Clean old caches
 self.addEventListener('activate', (event) => {
-    console.log('[SW] Activating Service Worker v3.0.5');
+    console.log('[SW] Activating Service Worker v3.1.0');
     event.waitUntil(
         caches.keys()
             .then((cacheNames) => {
@@ -150,4 +150,4 @@ self.addEventListener('message', (event) => {
     }
 });
 
-console.log('[SW] Service Worker v3.0.5 loaded');
+console.log('[SW] Service Worker v3.1.0 loaded');
